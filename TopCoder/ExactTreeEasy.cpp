70e2a9667946d268fe382adeae45619834598160
mkmostafa <mkmostafa92@gmail.com>
2015-09-18 18:10:29 +0300

    AC!!

---
+++ TopCoder/ExactTreeEasy.cpp
@@ -0,0 +1,223 @@
+// Paste me into the FileEdit configuration dialog
+
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <list>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <set>
+#include <fstream>
+#include <limits>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define rep(i,N) for(int i=0;i<N;i++)
+#define memclr(ARRAY) memset(ARRAY,0,sizeof(ARRAY))
+#define MAXPRIME 1000000007
+#define PI 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745
+
+bool compare(int a, int b)
+{
+	return a > b;
+}
+
+class Compare
+{
+	public:
+		bool operator()(int a, int b)
+		{
+			return a > b;
+		}
+};
+
+int mod(int a, int b)
+{
+	return ((a % b) + b) % b;
+}
+
+
+class ExactTreeEasy {
+public:
+	vector <int> getTree( int n, int m ) 
+	{
+		vector<int> tree;
+		int filler = n - m - 1;
+		tree.push_back(0), tree.push_back(1);
+		rep(i, filler) tree.push_back(i + 1), tree.push_back(i + 2); 
+		rep(i, m - 1) tree.push_back(filler + 1), tree.push_back(i + filler + 2);
+		return tree;
+	}
+};
+
+// BEGIN CUT HERE
+namespace moj_harness {
+	int run_test_case(int);
+	void run_test(int casenum = -1, bool quiet = false) {
+		if (casenum != -1) {
+			if (run_test_case(casenum) == -1 && !quiet) {
+				cerr << "Illegal input! Test case " << casenum << " does not exist." << endl;
+			}
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = run_test_case(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			cerr << "No test cases run." << endl;
+		} else if (correct < total) {
+			cerr << "Some cases FAILED (passed " << correct << " of " << total << ")." << endl;
+		} else {
+			cerr << "All " << total << " tests passed!" << endl;
+		}
+	}
+	
+	template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << "{"; for (typename vector<T>::const_iterator vi=v.begin(); vi!=v.end(); ++vi) { if (vi != v.begin()) os << ","; os << " " << *vi; } os << " }"; return os; }
+
+	int verify_case(int casenum, const vector <int> &expected, const vector <int> &received, clock_t elapsed) { 
+		cerr << "Example " << casenum << "... "; 
+		
+		string verdict;
+		vector<string> info;
+		char buf[100];
+		
+		if (elapsed > CLOCKS_PER_SEC / 200) {
+			sprintf(buf, "time %.2fs", elapsed * (1.0/CLOCKS_PER_SEC));
+			info.push_back(buf);
+		}
+		
+		if (expected == received) {
+			verdict = "PASSED";
+		} else {
+			verdict = "FAILED";
+		}
+		
+		cerr << verdict;
+		if (!info.empty()) {
+			cerr << " (";
+			for (int i=0; i<(int)info.size(); ++i) {
+				if (i > 0) cerr << ", ";
+				cerr << info[i];
+			}
+			cerr << ")";
+		}
+		cerr << endl;
+		
+		if (verdict == "FAILED") {
+			cerr << "    Expected: " << expected << endl; 
+			cerr << "    Received: " << received << endl; 
+		}
+		
+		return verdict == "PASSED";
+	}
+
+	int run_test_case(int casenum__) {
+		switch (casenum__) {
+		case 0: {
+			int n                     = 4;
+			int m                     = 2;
+			int expected__[]          = {0, 1, 1, 2, 2, 3 };
+
+			clock_t start__           = clock();
+			vector <int> received__   = ExactTreeEasy().getTree(n, m);
+			return verify_case(casenum__, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}
+		case 1: {
+			int n                     = 4;
+			int m                     = 3;
+			int expected__[]          = {0, 1, 1, 2, 1, 3 };
+
+			clock_t start__           = clock();
+			vector <int> received__   = ExactTreeEasy().getTree(n, m);
+			return verify_case(casenum__, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}
+		case 2: {
+			int n                     = 3;
+			int m                     = 2;
+			int expected__[]          = {0, 1, 1, 2 };
+
+			clock_t start__           = clock();
+			vector <int> received__   = ExactTreeEasy().getTree(n, m);
+			return verify_case(casenum__, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}
+		case 3: {
+			int n                     = 5;
+			int m                     = 3;
+			int expected__[]          = {0, 1, 1, 2, 1, 3, 3, 4 };
+
+			clock_t start__           = clock();
+			vector <int> received__   = ExactTreeEasy().getTree(n, m);
+			return verify_case(casenum__, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}
+		case 4: {
+			int n                     = 10;
+			int m                     = 9;
+			int expected__[]          = {0, 1, 1, 2, 1, 3, 1, 4, 1, 5, 1, 6, 1, 7, 1, 8, 1, 9 };
+
+			clock_t start__           = clock();
+			vector <int> received__   = ExactTreeEasy().getTree(n, m);
+			return verify_case(casenum__, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}
+
+		// custom cases
+
+/*      case 5: {
+			int n                     = ;
+			int m                     = ;
+			int expected__[]          = ;
+
+			clock_t start__           = clock();
+			vector <int> received__   = ExactTreeEasy().getTree(n, m);
+			return verify_case(casenum__, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}*/
+/*      case 6: {
+			int n                     = ;
+			int m                     = ;
+			int expected__[]          = ;
+
+			clock_t start__           = clock();
+			vector <int> received__   = ExactTreeEasy().getTree(n, m);
+			return verify_case(casenum__, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}*/
+/*      case 7: {
+			int n                     = ;
+			int m                     = ;
+			int expected__[]          = ;
+
+			clock_t start__           = clock();
+			vector <int> received__   = ExactTreeEasy().getTree(n, m);
+			return verify_case(casenum__, vector <int>(expected__, expected__ + (sizeof expected__ / sizeof expected__[0])), received__, clock()-start__);
+		}*/
+		default:
+			return -1;
+		}
+	}
+}
+ 
+
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		moj_harness::run_test();
+	} else {
+		for (int i=1; i<argc; ++i)
+			moj_harness::run_test(atoi(argv[i]));
+	}
+}
+// END CUT HERE
