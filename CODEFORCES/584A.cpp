1c509f90d57db9dec320a0865765bee0049b2517
mkmostafa <mkmostafa92@gmail.com>
2015-10-06 22:35:39 +0200

    584A

---
+++ CODEFORCES/584A.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <list>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <set>
+#include <fstream>
+#include <limits>
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define rep(i,N) for(int i=0;i<N;i++)
+#define memclr(ARRAY) memset(ARRAY,0,sizeof(ARRAY))
+#define MAXPRIME 1000000007
+#define PI 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745
+
+bool compare(int a, int b)
+{
+	return a > b;
+}
+
+class Compare
+{
+	public:
+		bool operator()(int a, int b)
+		{
+			return a > b;
+		}
+};
+
+int mod(int a, int b)
+{
+	return ((a % b) + b) % b;
+}
+
+int main()
+{
+	int n, t;
+	scanf("%d %d", &n, &t);
+	if(t != 10) rep(i, n) printf("%d", t);
+	else if(n < 2) printf("-1\n");
+	else
+	{
+		rep(i, n - 2) printf("1");
+		printf("10");
+	}
+	return 0;
+}
